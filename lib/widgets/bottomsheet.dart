00692e035ae4913eab7ed6fad7b92f1250c883c7
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-17 12:11:45 +0530

    setting up files

---
+++ lib/widgets/bottomsheet.dart
@@ -0,0 +1,35 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+
+ showbottomsheet(){
+   Get.bottomSheet(
+
+       Container(
+         decoration: BoxDecoration(
+             color: Colors.grey,
+             borderRadius: BorderRadius.circular(20)
+         ),
+         child: Column(
+           children: [
+             ListTile(
+               title:Text('Light theme'),
+               leading: Icon(Icons.light_mode),
+               onTap: (){
+                 Get.changeTheme(ThemeData.light());
+
+               },
+             ),
+             ListTile(
+               title:Text('Dark theme'),
+               leading: Icon(Icons.dark_mode),
+               onTap: (){
+                 Get.changeTheme(ThemeData.dark());
+
+               },
+             ),
+
+           ],
+         ),
+       )
+   );}
